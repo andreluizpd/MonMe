31e6b9c72e43a26c350adac65962f5255c6f43fe
André Luiz Pastore Dambros <andreluizpd@gmail.com>
2020-05-25 14:11:28 -0300

    Added routes for services

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+// Route Files
+const services = require('./routes/services');
+
 // Load Config File
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+// Mount routes
+app.use('/api/v1/services', services);
 
+const PORT = process.env.PORT || 5000;
 app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on ${PORT}`)
