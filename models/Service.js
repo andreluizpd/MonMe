3eee82b139ca025c8498303c870c42ecb361074f
André Luiz Pastore Dambros <andreluizpd@gmail.com>
2020-05-28 15:16:54 -0300

    Created service model

---
+++ models/Service.js
@@ -0,0 +1,64 @@
+const mongoose = require('mongoose');
+
+const ServiceSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please add a name'],
+    trim: true,
+    maxlength: [50, 'Name cannot be more than 50 characters'],
+  },
+  slug: String,
+  description: {
+    type: String,
+    required: [true, 'Please add a description'],
+    maxlength: [500, 'Description cannot be more than 500 characters'],
+  },
+  category: {
+    type: [String],
+    required: true,
+    enum: ['Electrician', 'Plumber', 'Other'],
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: {
+      type: String,
+      enum: ['Point'],
+      required: true,
+    },
+    coordinates: {
+      type: [Number],
+      required: true,
+      index: '2dsphere',
+    },
+    formattedAddress: String,
+    street: String,
+    city: String,
+    state: String,
+    zipcode: String,
+    country: String,
+  },
+  averageCost: Number,
+  available: Boolean,
+  averageRating: {
+    type: Number,
+    min: [1, 'Rating must be at least 1'],
+    max: [10, 'Rating can not be more than 10'],
+  },
+  contact: {
+    type: String,
+    maxlength: [20, 'Phone number can not be longer than 20 characters'],
+  },
+  photo: {
+    type: String,
+    default: 'no-photo.jpg',
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('Service', ServiceSchema);
